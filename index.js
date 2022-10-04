50e3869bdc67da923d9223765dbfc13954848b92
jmurphy5613 <jmurphy5613@gmail.com>
2022-10-04 14:00:32 -0400

    test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const guessRouter = require('./routes/Guess');
 app.use('/guess', guessRouter);
 
 db.sequelize.sync().then(
-    app.listen(process.env.PORT || port, () => {
+    app.listen(process.env.PORT, () => {
         console.log('LISTENN!!!!!');
     })
 );
